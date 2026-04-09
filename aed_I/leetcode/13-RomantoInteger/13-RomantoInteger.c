21d68eb3ba6e0b23e7e790861055dc8b2fa51bad
Arthur Ferreira Borges <145126589+arthurfborges@users.noreply.github.com>
2026-04-09 17:33:41 -0300

    File updated at 09/04/2026, 17:33:41

--- aed_I/leetcode/13-RomantoInteger/13-RomantoInteger.c
+++ aed_I/leetcode/13-RomantoInteger/13-RomantoInteger.c
@@ -1,37 +1,67 @@
-// Last updated: 09/04/2026, 17:27:52
+// Last updated: 09/04/2026, 17:33:41
 1int romanToInt(char* s) {
 2    int number = 0;
 3    int x = 0;
 4    int len = strlen(s);
 5
 6    while (x < len) {
-7        // Case: I
-8        if (s[x] == 'I') {
-9            if (s[x + 1] == 'V') { number += 4; x += 2; }
-10            else if (s[x + 1] == 'X') { number += 9; x += 2; }
-11            else { number += 1; x++; }
-12        } 
-13        // Case: X
-14        else if (s[x] == 'X') {
-15            if (s[x + 1] == 'L') { number += 40; x += 2; }
-16            else if (s[x + 1] == 'C') { number += 90; x += 2; }
-17            else { number += 10; x++; }
-18        } 
-19        // Case: C
-20        else if (s[x] == 'C') {
-21            if (s[x + 1] == 'D') { number += 400; x += 2; }
-22            else if (s[x + 1] == 'M') { number += 900; x += 2; }
-23            else { number += 100; x++; }
-24        } 
-25        // Single Character Cases
-26        else if (s[x] == 'V') { number += 5; x++; }
-27        else if (s[x] == 'L') { number += 50; x++; }
-28        else if (s[x] == 'D') { number += 500; x++; }
-29        else if (s[x] == 'M') { number += 1000; x++; }
-30        else {
-31            x++; // Fallback for unexpected characters
-32        }
-33    }
-34
-35    return number;
-36}
+7        if (s[x] == 'I'){
+8            if(s[x + 1] == 'V'){
+9                number += 4; 
+10                x += 2; 
+11            }
+12            else if(s[x + 1] == 'X'){
+13                number += 9; 
+14                x += 2; 
+15            }
+16            else{
+17                number += 1; 
+18                x++; 
+19            }
+20        } 
+21
+22        else if (s[x] == 'X'){
+23            if(s[x + 1] == 'L'){
+24                number += 40; 
+25                x += 2; 
+26            }
+27            else if (s[x + 1] == 'C'){
+28                number += 90; 
+29                x += 2; 
+30            }
+31            else{
+32                number += 10; 
+33                x++; 
+34            }
+35
+36        }else if (s[x] == 'C') {
+37            if(s[x + 1] == 'D'){
+38                number += 400; 
+39                x += 2;
+40            }
+41            else if(s[x + 1] == 'M'){
+42                number += 900; 
+43                x += 2;
+44            }
+45            else { 
+46                number += 100; 
+47                x++; 
+48            }
+49        } 
+50
+51        else if(s[x] == 'V'){
+52            number += 5; x++; 
+53        }
+54        else if(s[x] == 'L'){ 
+55            number += 50; x++; 
+56        }
+57        else if(s[x] == 'D'){
+58            number += 500; x++; 
+59        }
+60        else if(s[x] == 'M'){
+61             number += 1000; x++;
+62        }
+63    }
+64
+65    return number;
+66}
