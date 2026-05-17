3c97a01db361b45b04382fda8f4896faaef763b1
arthur_borges <arthurferreiraborges.2004@gmail.com>
2026-05-17 13:00:49 -0300

    doom formatting

--- aed_I/in_class/Av2/av2_casa_bruto.c
+++ aed_I/in_class/Av2/av2_casa_bruto.c
@@ -3,43 +3,42 @@
  */
 
 //recorde testecases: 77/223
-int* nextGreaterElements(int* nums, int numsSize, int* returnSize) {
+int* nextGreaterElements ( int* nums, int numsSize, int* returnSize ) {
     *returnSize = numsSize;
-    int* output = (int*)malloc(numsSize * sizeof(int));
-    if (!output){
-        printf("erro no malloc");
-        exit(1);
-    } 
+    int* output = ( int* ) malloc ( numsSize * sizeof ( int ) );    
+    if ( !output ) {
+        printf ( "erro no malloc" );
+        exit ( 1 );
+    }
+
     int x = 0;
     int check;
     
 
-    for(int i = 0; i < numsSize; i++){
-            output[i] = -1; // nao achou
-            x = i + 1; // pra nao verificar c ele mesmo
-            check =0;
+    for ( int i = 0; i < numsSize; i++ ) {
+        output[i] = -1; // nao achou
+        x = i + 1; // pra nao verificar c ele mesmo
+        check =0;
 
-        while(x  < numsSize){ 
-            if(nums[i] < nums[x]){
+        while ( x  < numsSize ) { 
+            if ( nums[i] < nums[x] ) {
                 output[i] = nums[x];
                 check= 1;
                 break;
-            }
-            
+            }   
             x++;
         } // agora percorre todo o array antes de verificar o check, ao inves de verifcar a cada iteraçao de i
 
-            if( !check ){ // volta p o inicio e vai ate o indice inicial
-                x=0;
-
-                while(x < i){ 
-                    if(nums[i] < nums[x]){
-                        output[i] = nums[x];
-                        break;
-                    }
-                    x++;
+        if ( !check ) { // volta p o inicio e vai ate o indice inicial
+            x=0;
+            while ( x < i ) { 
+                if ( nums[i] < nums[x] ) {
+                    output[i] = nums[x];
+                    break;
                 }
+                x++;
             }
+        }
     }
     return output;
 }
